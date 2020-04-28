384f1590203b7d7a8b9461f561b750da6000c531
Frank Ceballos <frank.ceballos123@gmail.com>
2020-04-28 11:41:27 -0500

    added I Am Poor App

---
+++ 02-I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Frank Ceballos on 4/28/20.
+//  Copyright © 2020 Frank Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
